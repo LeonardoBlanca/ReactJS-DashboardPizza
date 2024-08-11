74c52fe9817553281c5c23141782633b60ee3669
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2024-08-10 21:29:39 -0300

    feat: Added Dark theme and right menu in dashboard page
    - Right menu has two values: "Perfil da Loja" and "Sair"
    - Added Dark Theme button

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './global.css'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
   return (
     <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+
       <Toaster richColors/>
       <Helmet titleTemplate='%s | Pizza.Shop'/>
       <RouterProvider router={router} />
+      </ThemeProvider>
 
     </HelmetProvider>
   )
