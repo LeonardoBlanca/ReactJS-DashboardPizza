f4c09d654dccb58257dccd3f96d6b320c65b74f6
leonardoblanca <lssilvatati@gmail.com>
2024-08-12 23:59:04 -0300

    feat: Added pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/components/pagination";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
@@ -10,7 +11,7 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
+    
       <div className="space-y-2.5">
         <form className="flex items-center gap-2">
             <span className="text-sm font-semibold">Filtros:</span>
@@ -71,6 +72,9 @@ export function Orders() {
                 </TableBody>
                     
             </Table>
+            </div>
+
+            <Pagination pageIndex={0} totalCount={105} perPage={10}/>
         </div>
       </div>
     </>
