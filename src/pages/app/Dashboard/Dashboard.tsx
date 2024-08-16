87b349bf70893bc25cb85753c4a940111ca06abf
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2024-08-16 16:41:22 -0300

    :sparkles: Feat: Add first cards Dashboard screen

---
+++ src/pages/app/Dashboard/Dashboard.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from "react-helmet-async"
+import {MonthRevenueCard} from "@/pages/app/Dashboard/month-revenue-card.tsx";
+import {MonthOrdersAmountCard} from "@/pages/app/Dashboard/month-orders-amount-card.tsx";
+import {DayOrdersAmountCard} from "@/pages/app/Dashboard/day-orders-amount-card.tsx";
+import {MonthCanceledOrdersAmount} from "@/pages/app/Dashboard/month-canceled-orders-amount.tsx";
+
+export function Dashboard() {
+    return (
+        <>
+        <Helmet title="dashboard" />
+            <div className="flex flex-col gap-4">
+                <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+                <div className="grid grid-cols-4 gap-4">
+                    <MonthRevenueCard />
+                    <MonthOrdersAmountCard />
+                    <DayOrdersAmountCard />
+                    <MonthCanceledOrdersAmount />
+                </div>
+            </div>
+        </>
+    )
+}
