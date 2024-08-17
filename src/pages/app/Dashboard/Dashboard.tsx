c6dbfd3754040fa242a198c03741be758c0e8165
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2024-08-16 23:04:16 -0300

    :sparkles: Feat: Add graphic chart for best selling products

--- src/pages/app/Dashboard/Dashboard.tsx
+++ src/pages/app/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import {MonthOrdersAmountCard} from "@/pages/app/Dashboard/month-orders-amount-c
 import {DayOrdersAmountCard} from "@/pages/app/Dashboard/day-orders-amount-card.tsx";
 import {MonthCanceledOrdersAmount} from "@/pages/app/Dashboard/month-canceled-orders-amount.tsx";
 import {RevenueChart} from "@/pages/app/Dashboard/revenue-chart.tsx";
+import {PopularProductsChart} from "@/pages/app/Dashboard/popular-products-chart.tsx";
 
 export function Dashboard() {
     return (
@@ -21,6 +22,7 @@ export function Dashboard() {
 
                 <div className="grid grid-cols-9 gap-4">
                     <RevenueChart />
+                    <PopularProductsChart />
                 </div>
             </div>
         </>
