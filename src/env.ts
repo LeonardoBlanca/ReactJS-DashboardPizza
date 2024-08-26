3d2906044d4fc84e4e7f7557cf699105db5d4f89
Leonardo Blanca <145368092+LeonardoBlanca@users.noreply.github.com>
2024-08-26 12:30:40 -0300

    :sparkles: Feat: Add loading state Menu

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
-import z from 'zod';
+import z from "zod";
 
 const envSchema = z.object({
-    VITE_API_URL: z.string().url(),
-})
+  VITE_API_URL: z.string().url(),
+  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === "true"),
+});
 
 // import.meta.env é (é o process do Node) de onde vêm as variáveis de ambiente no Vite
 // Parse vai validar se o import tem a mesma estrutura que o envSchema que criamos
-export const env = envSchema.parse(import.meta.env)
+export const env = envSchema.parse(import.meta.env);
